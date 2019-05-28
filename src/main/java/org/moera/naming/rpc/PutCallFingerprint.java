b97de8c51df321c3f8c76b678470613a01952a0b
Shmuel Melamud <smelamud@redhat.com>
2019-05-28 19:31:22 +0300

    No need to sign deadline.

--- src/main/java/org/moera/naming/rpc/PutCallFingerprint.java
+++ src/main/java/org/moera/naming/rpc/PutCallFingerprint.java
@@ -8,19 +8,17 @@ public class PutCallFingerprint extends Fingerprint {
     public int generation;
     public byte[] updatingKey;
     public String nodeUri;
-    public long deadline;
     public byte[] signingKey;
     public long validFrom;
     public byte[] previousDigest;
 
-    public PutCallFingerprint(String name, int generation, byte[] updatingKey, String nodeUri, long deadline,
-                              byte[] signingKey, long validFrom, byte[] previousDigest) {
+    public PutCallFingerprint(String name, int generation, byte[] updatingKey, String nodeUri, byte[] signingKey,
+                              long validFrom, byte[] previousDigest) {
         super(0);
         this.name = name;
         this.generation = generation;
         this.updatingKey = updatingKey;
         this.nodeUri = nodeUri;
-        this.deadline = deadline;
         this.signingKey = signingKey;
         this.validFrom = validFrom;
         this.previousDigest = previousDigest;
