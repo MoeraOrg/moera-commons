5d8a17e9a514655e77c4dc2f7896f606f481ec46
Shmuel Melamud <smelamud@redhat.com>
2020-12-08 23:15:13 +0200

    Restoring private/public key on any curve. Creating an uncompressed form of public key.

--- src/main/java/org/moera/commons/crypto/CryptoUtil.java
+++ src/main/java/org/moera/commons/crypto/CryptoUtil.java
@@ -61,12 +61,24 @@ public class CryptoUtil {
         return rawKey;
     }
 
+    public static byte[] toUncompressedPublicKey(ECPublicKey publicKey) {
+        byte[] raw = toRawPublicKey(publicKey);
+        byte[] uncompressed = new byte[raw.length + 1];
+        uncompressed[0] = 0x04;
+        System.arraycopy(raw, 0, uncompressed, 1, raw.length);
+        return uncompressed;
+    }
+
     public static ECPublicKey toPublicKey(byte[] rawKey) {
+        return toPublicKey(rawKey, EC_CURVE);
+    }
+
+    public static ECPublicKey toPublicKey(byte[] rawKey, String curve) {
         byte[] x = new byte[PUBLIC_KEY_LENGTH / 2];
         byte[] y = new byte[PUBLIC_KEY_LENGTH / 2];
         System.arraycopy(rawKey, 0, x, 0, x.length);
         System.arraycopy(rawKey, x.length, y, 0, y.length);
-        ECParameterSpec parameterSpec = ECNamedCurveTable.getParameterSpec(EC_CURVE);
+        ECParameterSpec parameterSpec = ECNamedCurveTable.getParameterSpec(curve);
         ECPoint w = parameterSpec.getCurve().createPoint(decodeUnsigned(x), decodeUnsigned(y));
         ECPublicKeySpec keySpec = new ECPublicKeySpec(w, parameterSpec);
 
@@ -82,7 +94,11 @@ public class CryptoUtil {
     }
 
     public static ECPrivateKey toPrivateKey(byte[] rawKey) {
-        ECParameterSpec parameterSpec = ECNamedCurveTable.getParameterSpec(EC_CURVE);
+        return toPrivateKey(rawKey, EC_CURVE);
+    }
+
+    public static ECPrivateKey toPrivateKey(byte[] rawKey, String curve) {
+        ECParameterSpec parameterSpec = ECNamedCurveTable.getParameterSpec(curve);
         ECPrivateKeySpec keySpec = new ECPrivateKeySpec(decodeUnsigned(rawKey), parameterSpec);
         try {
             return (ECPrivateKey) KeyFactory.getInstance("EC", "BC").generatePrivate(keySpec);
