29eff5020f6028c497b48436b146295bee4f0920
Shmuel Melamud <smelamud@redhat.com>
2023-08-25 01:12:36 +0300

    Removed deprecated APIs.

--- src/main/java/org/moera/naming/rpc/NamingService.java
+++ src/main/java/org/moera/naming/rpc/NamingService.java
@@ -23,17 +23,8 @@ public interface NamingService {
 
     RegisteredNameInfo getCurrent(@JsonRpcParam("name") String name, @JsonRpcParam("generation") int generation);
 
-    @Deprecated
-    RegisteredNameInfo getCurrentForLatest(@JsonRpcParam("name") String name);
-
     RegisteredNameInfo getPast(@JsonRpcParam("name") String name, @JsonRpcParam("generation") int generation,
                                @JsonRpcParam("at") long at);
-
-    @Deprecated
-    RegisteredNameInfo getPastForLatest(@JsonRpcParam("name") String name, @JsonRpcParam("at") long at);
-
-    @Deprecated
-    boolean isFree(@JsonRpcParam("name") String name);
 
     boolean isFree(@JsonRpcParam("name") String name, @JsonRpcParam("generation") int generation);
 
